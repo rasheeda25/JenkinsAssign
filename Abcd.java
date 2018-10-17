b23bb51ddd45e9add024c24df212ba304305a57c
rasheeda25 <sheeda25.a@gmail.com>
2018-10-18 00:38:42 +0530

    First commit

---
+++ Abcd.java
@@ -0,0 +1,6 @@
+public class Abcd{
+public static void main()
+{
+	System.out.println("This is class Abcd");
+}
+}
---
+++ Pqrs.java
@@ -0,0 +1,6 @@
+public class Pqrs{
+public static void main()
+{
+	System.out.println("This is class Pqrs");
+}
+}
